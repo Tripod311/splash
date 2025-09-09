e6e39b7b323a690ba950b96050db2f3c1b4f5c73
Tripod <ind.tripod@gmail.com>
2025-09-09 07:09:39 +0200

    show warnings on repeating templates

--- src/templateCache.ts
+++ src/templateCache.ts
@@ -2,6 +2,10 @@ export default class TemplateCache {
 	private static templates: Record<string, Node> = {};
 
 	static compileTemplate (componentName: string, template: string): Node {
+		if (componentName in TemplateCache.templates) {
+			console.warn(`TemplateCache already contains template with name ${componentName}`);
+		}
+
 		const root = document.createElement("div");
 		root.innerHTML = template;
 		const compiled = root.firstElementChild as Node;
