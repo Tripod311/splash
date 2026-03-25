d6fb747a61e88a21cab15d7960320dfe120d3842
Tripod <ind.tripod@gmail.com>
2026-03-25 21:07:34 +0100

    fix

--- src/slot.ts
+++ src/slot.ts
@@ -57,7 +57,7 @@ export default class Slot {
 
 	push (component: Component) {
 		this.content.push(component);
-		if (this.content.length >= 1) {
+		if (this.content.length > 1) {
 			this.content[this.content.length - 2]!.DOMNode.after(component.DOMNode);
 		} else {
 			this.anchor.after(component.DOMNode);
